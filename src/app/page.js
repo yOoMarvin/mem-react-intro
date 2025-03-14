67c37d39564fa0b4c1be5e6d71228694b990a341
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2025-03-14 13:59:36 +0100

    Class 01: Intro to React

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 "use client";
 
+import Title from "@/components/Title";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main></main>
+      <main>
+        <Title text="Hello React 👋" />
+        <p className="text-center">Let's get started with React and Next.js</p>
+        <Title text="Let's go" />
+      </main>
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,3 @@
+export default function Title(props) {
+  return <h1 className="text-center text-lg font-bold">{props.text}</h1>;
+}
