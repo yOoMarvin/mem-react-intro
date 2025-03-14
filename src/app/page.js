a5e8d333b9b65092af95a7f43adb36a114596620
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2025-03-14 14:21:26 +0100

    Final prep

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
 "use client";
 
-import ButtonDemo from "@/components/ButtonDemo";
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
